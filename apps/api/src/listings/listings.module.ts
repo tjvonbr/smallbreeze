aaf09a3dc74e44afbb5e19abb381fe8dab8810c9
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2026-02-08 22:28:57 -0700

    add bullmq along with some basic reservation update tasks

--- apps/api/src/listings/listings.module.ts
+++ apps/api/src/listings/listings.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bullmq';
 import { ListingsService } from './listings.service.js';
 import { ListingsController } from './listings.controller.js';
+import { NewReservationProcessor } from './new-reservation.processor.js';
 
 @Module({
+  imports: [BullModule.registerQueue({ name: 'new-reservation' })],
   controllers: [ListingsController],
-  providers: [ListingsService],
+  providers: [ListingsService, NewReservationProcessor],
 })
 export class ListingsModule {}
